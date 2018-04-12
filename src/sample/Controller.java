fb4ed17775443f4b1586c82f397df69cd42639e5
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2018-04-11 18:52:48 -0700

    Minor Changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,14 +2,29 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class Controller {
 
-    @FXML private Text actiontarget;
+    @FXML private Button actiontarget;
 
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Sign in button pressed");
+    @FXML protected void gameButtonPressed(ActionEvent event) {
+        actiontarget.setStyle("-fx-background-color: #000000");
     }
 
+    public Controller() {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        try {
+            Pane p = fxmlLoader.load(getClass().getResource("sample.fxml").openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Button a1 = (Button) fxmlLoader.getController();
+    }
 }
