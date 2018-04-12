4c3b49a52cc07b3133d8987e53e241be2146f7a5
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2018-04-11 18:41:12 -0700

    Controller Changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class Controller {
 
-    public Controller() {
-        @FXML private Text actiontarget;
+    @FXML private Text actiontarget;
 
-        @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-            actiontarget.setText("Sign in button pressed");
-        }
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
     }
 
 }
